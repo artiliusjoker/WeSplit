2f0d79031e4d7ffd0e70ecf67902e37533d58f14
Minh Quan <1712695@student.hcmus.edu.vn>
2020-12-06 22:21:39 +0700

    Create DatabaseEntity.cs

---
+++ WeSplit/Models/DatabaseEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeSplit.Models
+{
+    class DatabaseEntity
+    {
+		private static DatabaseEntity _Entity;
+		public static DatabaseEntity Entity
+		{
+			get
+			{
+				if (_Entity == null)
+					_Entity = new DatabaseEntity();
+				return _Entity;
+			}
+			set
+			{
+				_Entity = value;
+			}
+		}
+		public WESPLITEntities DB { get; set; }
+		private DatabaseEntity()
+		{
+			DB = new WESPLITEntities();
+		}
+	}
+}
