18aa9d025b46712c67c6009ad333f1c8be61e0a6
Minh Quan <1712695@student.hcmus.edu.vn>
2020-12-07 16:34:39 +0700

    add pagination

--- WeSplit/Helpers/Pagination.cs
+++ WeSplit/Helpers/Pagination.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WeSplit.Helpers
 {
-    class Pagination
+    public class PageInfo
     {
+        public int Page { get; set; }
+        public int TotalPages { get; set; }
+    }
+    class Pagination : INotifyPropertyChanged
+    {
+        public int CurrentPage { get; set; }
+        public int RowsPerPage { get; set; } = 4;
+
+        private int totalPages;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public int TotalPages
+        {
+            get => totalPages; 
+            set
+            {
+                totalPages = value;
+                Pages = new List<PageInfo>();
+                for (int i = 1; i <= totalPages; i++)
+                {
+                    Pages.Add(new PageInfo()
+                    {
+                        Page = i,
+                        TotalPages = totalPages
+                    });
+                }
+                OnPropertyChanged();
+            }
+        }
+        public List<PageInfo> Pages { get; set; }
     }
 }
--- WeSplit/ViewModel/CurrentTripsViewModel.cs
+++ WeSplit/ViewModel/CurrentTripsViewModel.cs
@@ -1,28 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using WeSplit.Models;
 
 namespace WeSplit.ViewModel
 {
     class CurrentTripsViewModel : BaseViewModel
     {
+        private ObservableCollection<Trip> bindingItems;
+        public ObservableCollection<Trip> BindingItems
+        {
+            get
+            {
+                if (bindingItems == null)
+                {
+                    bindingItems = new ObservableCollection<Trip>(Trips.ToList());
+                }
+                return bindingItems;
+            }
+            set
+            {
+                OnPropertyChanged(ref bindingItems, value);
+            }
+        }
         // danh sach chuyen di
         private List<Trip> _trips = null;
         public List<Trip> Trips
         {
             get
-            {
-                if (_trips == null)
-                {
-                    _trips = TripService.GetOngoingTrips();
-                }
+            {             
                 return _trips;
             }
             set { OnPropertyChanged(ref _trips, value); }
         }
+
+        #region Pagination
+
+        private int currentIndex;
+        public int CurrentIndex
+        {
+            get => this.currentIndex;
+            set
+            {
+                OnPropertyChanged(ref currentIndex, value);
+                CollectTrips();
+            }
+        }
+
+        private Helpers.Pagination paging = new Helpers.Pagination();
+
+        private void CollectTrips()
+        {
+            int page = this.CurrentIndex + 1;
+            int skip = (page - 1) * this.paging.RowsPerPage;
+            int take = this.paging.RowsPerPage;
+
+            BindingItems = new ObservableCollection<Trip>(this.Trips.Skip(skip)
+                                                                   .Take(take)
+                                                                   .ToList());
+        }
+
+        public ICommand NextPageCommand { get; set; }
+        public ICommand PreviousPageCommand { get; set; }
+
+        public CurrentTripsViewModel()
+        {
+            NextPageCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                if (this.CurrentIndex < this.paging.TotalPages - 1)
+                {
+                    this.CurrentIndex += 1;
+                }
+            });
+            PreviousPageCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                if (this.CurrentIndex > 0)
+                {
+                    this.CurrentIndex -= 1;
+                }
+            });
+
+            Trips = TripService.GetOngoingTrips();
+
+            int count = this._trips.Count();
+            int rowsPerPage = this.paging.RowsPerPage;
+
+            // Calculate paging info
+            paging = new Helpers.Pagination()
+            {
+                CurrentPage = 1,
+                TotalPages = count / rowsPerPage +
+                    (((count % rowsPerPage) == 0) ? 0 : 1)
+            };
+            this.CurrentIndex = 0;
+        }
+        #endregion
     }
 }
