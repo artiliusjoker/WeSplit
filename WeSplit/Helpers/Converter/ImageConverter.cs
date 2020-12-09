486072338fbffbdb75d6ac8360c9713cdca04ce5
Minh Quan <1712695@student.hcmus.edu.vn>
2020-12-09 11:14:52 +0700

    add image converter

---
+++ WeSplit/Helpers/Converter/ImageConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WeSplit.Helpers.Converter
+{
+    class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string imgPath = (string)value;
+            imgPath = imgPath.Replace('/' , '\\');
+            var baseFolder = AppDomain.CurrentDomain.BaseDirectory;
+            var absolute = $"{baseFolder}{imgPath}";
+            return absolute;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
