8e70b02b69216664b9115e1d628888f929c20e88
Minh Quan <1712695@student.hcmus.edu.vn>
2020-12-13 14:10:17 +0700

    add LongString converter

---
+++ WeSplit/Helpers/Converter/LongStringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WeSplit.Helpers.Converter
+{
+    class LongStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string longString = (string)value;
+            return longString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
