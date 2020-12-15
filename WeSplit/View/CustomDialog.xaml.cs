cf5b8aa8ec6a340e884531815421ff95ed9a69fd
Minh Quan <1712695@student.hcmus.edu.vn>
2020-12-15 22:25:52 +0700

    add custom dialog

---
+++ WeSplit/View/CustomDialog.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Media.Animation;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WeSplit.View
+{
+    /// <summary>
+    /// Interaction logic for CustomDialog.xaml
+    /// </summary>
+    public partial class CustomDialog : Window
+    {
+        public CustomDialog()
+        {
+            InitializeComponent();
+        }
+
+        public CustomDialog(string Text, ViewModel.CustomDialog.Buttons buttons)
+        {
+            InitializeComponent();
+
+            txbText.Text = Text;
+
+            switch (buttons)
+            {
+                case ViewModel.CustomDialog.Buttons.OK:
+                    btnOK.Visibility = Visibility.Visible;
+                    break;
+                case ViewModel.CustomDialog.Buttons.Yes_No:
+                    btnYes.Visibility = Visibility.Visible;
+                    btnNo.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        public string ReturnString { get; set; }
+
+        private void gBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch { }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            ReturnString = "-1";
+            Close();
+        }
+
+        DoubleAnimation anim;
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Closing -= Window_Closing;
+            e.Cancel = true;
+            anim = new DoubleAnimation(0, (Duration)TimeSpan.FromSeconds(0.3));
+            anim.Completed += (s, _) => this.Close();
+            this.BeginAnimation(UIElement.OpacityProperty, anim);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Height = (txbText.LineCount * 40) + gBar.Height + 60;
+        }
+
+        private void btnReturnValue_Click(object sender, RoutedEventArgs e)
+        {
+            ReturnString = ((Button)sender).Uid.ToString();
+            Close();
+        }
+    }
+}
---
+++ WeSplit/ViewModel/CustomDialog.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Media.Effects;
+using WeSplit.View;
+
+namespace WeSplit.ViewModel
+{
+    public class CustomDialog
+    {
+        public enum Buttons { Yes_No, OK }
+
+        public static string Show(string Text)
+        {
+            return Show(Text, Buttons.OK);
+        }
+
+        public static string Show(string Text, Buttons buttons)
+        {
+            View.CustomDialog messageBox = new View.CustomDialog(Text, buttons);
+            messageBox.Show();
+            return messageBox.ReturnString;
+        }
+
+        public static string ShowDialog(string Text)
+        {
+            return ShowDialog(Text, Buttons.OK);
+        }
+
+        public static string ShowDialog(string Text, Buttons buttons)
+        {
+            ShowBlurEffectAllWindow();
+            View.CustomDialog messageBox = new View.CustomDialog(Text, buttons);
+            messageBox.ShowDialog();
+            StopBlurEffectAllWindow();
+            return messageBox.ReturnString;
+        }
+
+        static BlurEffect MyBlur = new BlurEffect();
+        public static void ShowBlurEffectAllWindow()
+        {
+            MyBlur.Radius = 20;
+            foreach (Window window in Application.Current.Windows)
+                window.Effect = MyBlur;
+        }
+
+        public static void StopBlurEffectAllWindow()
+        {
+            MyBlur.Radius = 0;
+        }
+    }
+}
